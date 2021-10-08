4942978d32d89a7d22ab0d3a4b310c38b6316c9c
Thilo Maier <git@maier.asia>
2021-10-08 16:51:47 -0400

    Improve layout for category pages Fixes #64

--- src/lib/data/tags.ts
+++ src/lib/data/tags.ts
@@ -1,24 +1,32 @@
 import type { Tag } from '$lib/types/tag.type';
 
+import { base } from '$app/paths';
+
 // Permitted tags, sorted by key.
 const tags: Tag[] = [
+  {
+    key: 'seo',
+    label: 'SEO',
+    title: 'Posts about SEO',
+    path: `${base}/tags/seo`,
+  },
   {
     key: 'svelte',
     label: 'Svelte',
     title: 'Posts about Svelte',
-    path: '/tags/svelte',
+    path: `${base}/tags/svelte`,
   },
   {
     key: 'tooling',
     label: 'Tooling',
     title: 'Posts about developer tooling',
-    path: '/tags/tooling',
+    path: `${base}/tags/tooling`,
   },
   {
     key: 'vscode',
     label: 'VSCode',
     title: 'Posts about Visual Studio Code',
-    path: '/tags/vscode',
+    path: `${base}/tags/vscode`,
   },
 ];
 
