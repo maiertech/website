e087975be472983d092a6f4b03bd01162f46654e
Thilo Maier <github@maier.asia>
2022-01-10 03:30:06 +0000

    Fix HMR on Gitpod Fixes #223

--- svelte.config.js
+++ svelte.config.js
@@ -18,6 +18,16 @@ const config = {
   kit: {
     adapter: adapter(),
     target: '#svelte',
+    vite: {
+      server: {
+        hmr: {
+          clientPort: process.env.HMR_HOST ? 443 : 3000,
+          host: process.env.HMR_HOST
+            ? process.env.HMR_HOST.substring('https://'.length)
+            : 'localhost',
+        },
+      },
+    },
   },
 };
 
