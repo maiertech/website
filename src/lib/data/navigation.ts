5e9a7ce1cbd3132bd63d096730f2970a4ce6b424
Thilo Maier <github@maier.asia>
2022-02-01 20:32:20 +0000

    Add list with Tailwind designs Fixes #284

--- src/lib/data/navigation.ts
+++ src/lib/data/navigation.ts
@@ -1,9 +1,6 @@
 import type { Link } from '$lib/types/link.type';
 import { base } from '$app/paths';
 
-const navigation: Link[] = [
-  { title: 'Posts', href: `${base}/posts` },
-  { title: 'Svelte examples', href: `${base}/svelte/examples` },
-];
+const navigation: Link[] = [{ title: 'Posts', href: `${base}/posts` }];
 
 export default navigation;
