4ef041ffd5c51e455175076ad6efec2cd212add1
Thilo Maier <github@maier.asia>
2023-02-02 21:03:48 +0000

    Post: StackBlitz Codeflow Beta Closes #589

---
+++ src/routes/posts/[slug=stackblitz_codeflow]/+page.server.js
@@ -0,0 +1,9 @@
+import { normalize } from '$lib/utils/posts';
+import { metadata } from './+page.svx';
+
+/** @type {import('./$types').PageServerLoad} */
+export const load = async function ({ url }) {
+	const post = normalize(metadata, url.pathname);
+
+	return { post };
+};
