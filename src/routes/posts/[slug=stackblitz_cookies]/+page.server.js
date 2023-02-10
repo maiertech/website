917d1d962406596d3d59bf11f7fb1cb67ec38730
Thilo Maier <github@maier.asia>
2023-02-10 23:02:51 +0000

    Post: Cookie settings for StackBlitz WebContainers Closes #613

---
+++ src/routes/posts/[slug=stackblitz_cookies]/+page.server.js
@@ -0,0 +1,8 @@
+import { normalize } from '$lib/utils/posts';
+import { metadata } from './+page.svx';
+
+/** @type {import('../[slug=stackblitz_codeflow]/$types').PageServerLoad} */
+export const load = async function ({ url }) {
+	const post = normalize(metadata, url.pathname);
+	return { post };
+};
