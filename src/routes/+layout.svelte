29f601d0b53b679fabf3604c4b638494543c7a0b
Thilo Maier <github@maier.asia>
2024-03-17 21:41:49 +0000

    Replace Fathom Analytics with Vercel Analytics Fixes #812

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,18 @@
 <script>
-	import { browser } from '$app/environment';
+	import { dev } from '$app/environment';
 	import { page } from '$app/stores';
-	import { PUBLIC_FATHOM_SITE_ID } from '$env/static/public';
 	import Container from '$lib/components/container.svelte';
 	import Favicon from '$lib/components/favicon.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	import Header from '$lib/components/header.svelte';
 	import RootLayout from '$lib/components/root-layout.svelte';
 	import Seo from '$lib/components/seo.svelte';
-	import * as Fathom from 'fathom-client';
-	import { onMount } from 'svelte';
+	import { inject } from '@vercel/analytics';
 	import '../app.css';
-
-	// onMount runs client-side only.
-	onMount(() => {
-		Fathom.load(PUBLIC_FATHOM_SITE_ID);
-	});
 
 	export let data;
 
-	// Track page view when path changes.
-	$: $page.url.pathname, browser && Fathom.trackPageview();
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 {#if !$page.error}
