5ce9bbc1ccf445dbdd10aceb08d5782a5377a2be
Thilo Maier <git@maier.asia>
2024-12-14 16:34:52 +0100

    Migrate author component

--- src/lib/components/author.svelte
+++ src/lib/components/author.svelte
@@ -1,45 +1,18 @@
-<script>
+<script lang="ts">
 	import { Image } from '@maiertech/sveltekit-helpers';
+	import type { Author } from '@maiertech/sveltekit-helpers';
 
-	/** @type {import('zod').z.infer<typeof import('@maiertech/sveltekit-helpers').authorSchema>} */
-	export let value;
+	interface Props {
+		value: Author;
+		large?: boolean;
+	}
 
-	/** @type {boolean} */
-	export let large = false;
+	let { value, large = false }: Props = $props();
 </script>
 
-<div class="author" class:large>
-	<div class="avatar">
+<div class="flex items-center gap-2 font-semibold {large ? 'text-lg' : 'text-base'}">
+	<div class="overflow-hidden rounded-full {large ? 'h-9 w-9' : 'h-7 w-7'}">
 		<Image alt={value.name} src={value.imageUrl} aspectRatio={1 / 1} />
 	</div>
 	<p>{value.name}</p>
 </div>
-
-<style>
-	.author {
-		display: flex;
-		align-items: center;
-		gap: var(--size-2);
-		font-size: var(--font-size-1);
-		font-family: var(--font-sans);
-		font-weight: var(--font-weight-6);
-
-		&.large {
-			font-size: var(--font-size-2);
-		}
-	}
-
-	.avatar {
-		inline-size: var(--size-6);
-		block-size: var(--size-6);
-		border-radius: var(--radius-round);
-		overflow: hidden;
-	}
-
-	.large {
-		& .avatar {
-			inline-size: var(--size-7);
-			block-size: var(--size-7);
-		}
-	}
-</style>
