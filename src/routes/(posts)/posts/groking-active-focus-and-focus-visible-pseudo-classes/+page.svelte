f3056b5357b60d5701cbba89ed4be426bf85a092
Thilo Maier <git@maier.asia>
2024-12-24 23:15:45 +0000

    Groking focus post

--- src/routes/(posts)/posts/groking-active-focus-and-focus-visible-pseudo-classes/+page.svelte
+++ src/routes/(posts)/posts/groking-active-focus-and-focus-visible-pseudo-classes/+page.svelte
@@ -1,8 +1,14 @@
-<script>
-	import StackblitzEmbed from '$lib/components/stackblitz-embed.svelte';
-	import { Image } from '@maiertech/sveltekit-helpers';
-	import Highlight from 'svelte-highlight';
-	import { css } from 'svelte-highlight/languages';
+<script lang="ts">
+	import { StackblitzEmbed } from '$lib/components';
+	import {
+		code as Code,
+		Figure,
+		h2 as H2,
+		Image,
+		p as P,
+		Shiki
+	} from '@maiertech/sveltekit-helpers';
+	import type { PageData } from './$types';
 	import active_example_button_active_origin_image from './active-example-button-active.png';
 	import active_example_input_active_origin_image from './active-example-input-active.png';
 	import active_example_input_focus_origin_image from './active-example-input-focus.png';
@@ -12,225 +18,231 @@
 	import focus_visible_example_button_mouse_origin_image from './focus-visible-example-button-mouse.png';
 	import focus_visible_example_input_origin_image from './focus-visible-example-input.png';
 
-	export let data;
+	interface Props {
+		data: PageData;
+	}
+
+	let { data }: Props = $props();
 </script>
 
-<p>
+<P>
 	Recently, I worked on the newsletter sign-up form for this website. When I looked at examples, I
 	saw that other developers used pseudo-classes
 	<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/:active">:active</a>,
 	<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/:focus">:focus</a>
 	<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/:focus-visible">:focus-visible</a> to style
 	form fields.
-</p>
+</P>
 
-<p>
+<P>
 	My handle on these pseudo-classes could have been better, and I decided to read up on their
 	specifications. In this post, I share my learnings. You can follow along with the StackBlitz
 	workspace below. It also runs on mobile devices, but you should run it in a desktop browser to get
-	the most out of this post. For instance, pseudo-class <code>:active</code> does not affect touch devices,
+	the most out of this post. For instance, pseudo-class <Code>:active</Code> does not affect touch devices,
 	and the first example will make no sense.
-</p>
+</P>
 
-<p>
+<P>
 	I took the screenshots in this post with Google Chrome. What you see in your browser may be
 	different. There are subtle cross-browser differences for the pseudo-classes covered in this post.
-</p>
+</P>
 
-<StackblitzEmbed project={data.example.project} options={data.example.options} />
+<Figure class="mb-6">
+	<StackblitzEmbed project={data.example.project} options={data.example.options} />
+</Figure>
 
-<h2>Pseudo-class :active</h2>
+<H2>Pseudo-class :active</H2>
 
-<p>
-	A browser applies <code>:active</code> when a user activates an element. Activation refers to the
-	timespan between clicking an element and releasing the mouse button. <code>:active</code> can be
-	applied to elements with which users interact, e.g., <code>&lt;a&gt;</code>,
-	<code>&lt;button&gt;</code>, <code>&lt;input&gt:</code>, <code>&lt;select&gt;</code>, and
-	<code>&lt;textarea&gt;</code>.
-</p>
+<P>
+	A browser applies <Code>:active</Code> when a user activates an element. Activation refers to the timespan
+	between clicking an element and releasing the mouse button. <Code>:active</Code> can be applied to
+	elements with which users interact, e.g., <Code>&lt;a&gt;</Code>,
+	<Code>&lt;button&gt;</Code>, <Code>&lt;input&gt:</Code>, <Code>&lt;select&gt;</Code>, and
+	<Code>&lt;textarea&gt;</Code>.
+</P>
 
-<p>
-	In the first example we look at <code>:active</code>. I applied <code>:active</code> to the input element
+<P>
+	In the first example we look at <Code>:active</Code>. I applied <Code>:active</Code> to the input element
 	and submit button with the following styles:
-</p>
+</P>
 
-<figure>
-	<Highlight
-		language={css}
+<Figure class="mb-6">
+	<Shiki
+		lang="css"
 		code={`input:active,
 button:active {
   outline: dashed red;
   outline-offset: 1px;
 }`}
 	/>
-</figure>
+</Figure>
 
-<p>
+<P>
 	When you click the input field, you can see a dashed red outline until you release the mouse
 	button:
-</p>
+</P>
 
-<figure>
+<Figure class="mb-6">
 	<Image
 		src={active_example_input_active_origin_image}
 		alt="A simple web form with a name input field and a submit button. The name input field is active and highlighted with a red dashed outline."
 	/>
-</figure>
+</Figure>
 
-<p>
+<P>
 	When you release the mouse button, the input field has focus. The browser draws a default outline
 	around it to highlight its focus:
-</p>
+</P>
 
-<figure>
+<Figure class="mb-6">
 	<Image
 		src={active_example_input_focus_origin_image}
 		alt="A simple web form with a name input field and a submit button. The name input field has focus, and the browser applies a blue outline to it."
 	/>
-</figure>
+</Figure>
 
-<p>While you click the submit button, you can see the dashed red outline until you release it:</p>
+<P>While you click the submit button, you can see the dashed red outline until you release it:</P>
 
-<figure>
+<Figure class="mb-6">
 	<Image
 		src={active_example_button_active_origin_image}
 		alt="A simple web form with a name input field and a submit button. The submit button is highlighted with a dashed red outline."
 	/>
-</figure>
+</Figure>
 
-<p>
-	The takeaway from this example is that <code>:active</code> styles are in effect while you press the
+<P>
+	The takeaway from this example is that <Code>:active</Code> styles are in effect while you press the
 	mouse button until you release it.
-</p>
+</P>
 
-<h2>Pseudo-class :focus</h2>
+<H2>Pseudo-class :focus</H2>
 
-<p>
-	A browser applies <code>:focus</code> when the activation is complete, and an element is ready for
+<P>
+	A browser applies <Code>:focus</Code> when the activation is complete, and an element is ready for
 	user interaction. There are different ways of giving focus to an element. You can click or tap the
 	element or use keyboard navigation with the <key>Tab</key> key.
-</p>
+</P>
 
-<p>
-	In the second example, we look at <code>:focus</code>. I applied <code>:focus</code> to the input element
+<P>
+	In the second example, we look at <Code>:focus</Code>. I applied <Code>:focus</Code> to the input element
 	and submit button with the same styles as before:
-</p>
+</P>
 
-<figure>
-	<Highlight
-		language={css}
+<Figure class="mb-6">
+	<Shiki
+		lang="css"
 		code={`input:focus,
 button:focus {
   outline: dashed red;
   outline-offset: 1px;
 }`}
 	/>
-</figure>
+</Figure>
 
-<p>
+<P>
 	You can see the dashed red outline again when you click the input field. But this time, it stays
 	until the input field loses focus:
-</p>
+</P>
 
-<figure>
+<Figure class="mb-6">
 	<Image
 		src={focus_example_input_origin_image}
 		alt="A simple web form with a name input field and a submit button. The input field is focused and highlighted with a dashed red outline."
 	/>
-</figure>
+</Figure>
 
-<p>
+<P>
 	Likewise, when you click the submit button, you can see the dashed outline until it loses focus:
-</p>
+</P>
 
-<figure>
+<Figure class="mb-6">
 	<Image
 		src={focus_example_button_origin_image}
 		alt="A simple web form with a name input field and a submit button. The submit button has focus and the browser applied a dashed red outline to it."
 	/>
-</figure>
+</Figure>
 
-<p>
+<P>
 	Instead of clicking the input field and submit button, you can use the <key>Tab</key> key to navigate
 	between elements and focus them. Try it out in StackBlitz.
-</p>
+</P>
 
-<p>
-	The takeaway from this example is that <code>:focus</code> styles are applied when an element has
-	focus, and it does not matter whether a click, tap, or the <key>Tab</key> key triggers the focus.
-</p>
+<P>
+	The takeaway from this example is that <Code>:focus</Code> styles are applied when an element has focus,
+	and it does not matter whether a click, tap, or the <key>Tab</key> key triggers the focus.
+</P>
 
-<h2>Pseudo-class :focus-visible</h2>
+<H2>Pseudo-class :focus-visible</H2>
 
-<p>
-	<code>:focus-visible</code> can only be applied when an element has focus. But a browser can
-	decide whether or not to apply <code>:focus-visible</code> to an element with focus. Browsers use
-	a heuristic to determine whether they apply <code>:focus-visible</code> to a focused element.
-</p>
+<P>
+	<Code>:focus-visible</Code> can only be applied when an element has focus. But a browser can decide
+	whether or not to apply <Code>:focus-visible</Code> to an element with focus. Browsers use a heuristic
+	to determine whether they apply <Code>:focus-visible</Code> to a focused element.
+</P>
 
-<p>
-	In the third example, we look at <code>:focus-visible</code>. I applied
-	<code>:focus-visible</code> to the input element and submit button with the same styles as in the previous
+<P>
+	In the third example, we look at <Code>:focus-visible</Code>. I applied
+	<Code>:focus-visible</Code> to the input element and submit button with the same styles as in the previous
 	examples:
-</p>
+</P>
 
-<figure>
-	<Highlight
-		language={css}
+<Figure class="mb-6">
+	<Shiki
+		lang="css"
 		code={`input:focus-visible,
 button:focus-visible {
   outline: dashed red;
   outline-offset: 1px;
 }`}
 	/>
-</figure>
+</Figure>
 
-<p>
+<P>
 	When you click the input field, you can see the dashed red outline again when the input field has
 	focus:
-</p>
+</P>
 
-<figure>
+<Figure class="mb-6">
 	<Image
 		src={focus_visible_example_input_origin_image}
 		alt="A simple web form with a name input field and a submit button. The name input field has focus, and the browser applied :focus-visible and a dashed red outline."
 	/>
-</figure>
+</Figure>
 
-<p>
-	For the input field, there is no difference between <code>:focus</code> and
-	<code>:focus-visible</code>. But when you click the submit button, the dashed red outline is not
+<P>
+	For the input field, there is no difference between <Code>:focus</Code> and
+	<Code>:focus-visible</Code>. But when you click the submit button, the dashed red outline is not
 	visible:
-</p>
+</P>
 
-<figure>
+<Figure class="mb-6">
 	<Image
 		src={focus_visible_example_button_mouse_origin_image}
 		alt="A simple web form with a name input field and a submit button. Even though the submit has focus, the browser did not apply :focus-visible and the corresponding styling."
 	/>
-</figure>
+</Figure>
 
-<p>
+<P>
 	But when you use the <key>Tab</key> key to focus the submit button, the dashed red outline is visible:
-</p>
+</P>
 
-<figure>
+<Figure class="mb-6">
 	<Image
 		src={focus_visible_example_button_keyboard_origin_image}
 		alt="A simple web form with a name input field and a submit button. This time, the browser applied :focus-visible and a dashed red outline to the submit button."
 	/>
-</figure>
+</Figure>
 
-<p>
-	In this case, the browser decided against applying <code>:focus-visible</code> to an element with
-	focus. And it makes sense. Once the input field is focused, the user must type in some
-	information, and the styles applied with <code>:focus-visible</code> emphasize this expectation.
-</p>
+<P>
+	In this case, the browser decided against applying <Code>:focus-visible</Code> to an element with focus.
+	And it makes sense. Once the input field is focused, the user must type in some information, and the
+	styles applied with <Code>:focus-visible</Code> emphasize this expectation.
+</P>
 
-<p>
+<P>
 	The submit button is a different story. After a user clicks the button, no other interaction is
-	required; thus, the browser does not apply <code>:focus-visible</code>. For most common use cases
-	you will encounter as a developer, it makes sense to use the <code>:focus-visible</code>
+	required; thus, the browser does not apply <Code>:focus-visible</Code>. For most common use cases
+	you will encounter as a developer, it makes sense to use the <Code>:focus-visible</Code>
 	pseudo-class instead of
-	<code>:focus</code>.
-</p>
+	<Code>:focus</Code>.
+</P>
