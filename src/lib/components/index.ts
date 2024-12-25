343797e96ce5e625af981956a76efc9beb0588c5
Thilo Maier <git@maier.asia>
2024-12-25 23:17:35 +0000

    Migrate route matching post

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Button } from './button.svelte';
-export { default as Card } from './card.svelte';
 export { default as Favicon } from './favicon.svelte';
 export { default as Footer } from './footer.svelte';
 export { default as Header } from './header.svelte';
