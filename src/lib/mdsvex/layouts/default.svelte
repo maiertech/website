dadfba3f7838be0d74e993544d4030532cec4ffc
Thilo Maier <git@maier.asia>
2024-12-26 17:00:37 +0000

    Update Mdsvex layout

--- src/lib/mdsvex/layouts/default.svelte
+++ src/lib/mdsvex/layouts/default.svelte
@@ -1,21 +1,21 @@
 <script lang="ts" module>
 	// Custom components for Markdown rendering
 	import {
-		code,
-		h1,
-		h2,
-		h3,
-		h4,
-		h5,
-		h6,
-		img,
-		ol,
-		p,
-		table,
-		td,
-		th,
-		thead,
-		ul
+		Code as code,
+		H1 as h1,
+		H2 as h2,
+		H3 as h3,
+		H4 as h4,
+		H5 as h5,
+		H6 as h6,
+		Img as img,
+		Ol as ol,
+		P as p,
+		Table as table,
+		Td as td,
+		Th as th,
+		Thead as thead,
+		Ul as ul
 	} from '@maiertech/sveltekit-helpers';
 	// eslint-disable-next-line no-import-assign
 	export { code, h1, h2, h3, h4, h5, h6, img, ol, p, table, td, th, thead, ul };
