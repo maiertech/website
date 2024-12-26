bf52e348d803972afbd3dd06d13dc535d5df6abb
Thilo Maier <git@maier.asia>
2024-12-26 00:42:09 +0000

    Unlink Newsletter

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Button } from './button.svelte';
 export { default as Favicon } from './favicon.svelte';
 export { default as Footer } from './footer.svelte';
 export { default as Header } from './header.svelte';
