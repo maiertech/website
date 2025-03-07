d544ec2e21eb5570bf08684ed0061b78005335c3
Thilo Maier <github@maier.asia>
2025-03-07 17:27:59 +0100

    First shot at notes (#909)

---
+++ src/lib/schemas/note-metadata-schema.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export default z.object({
+	title: z.string(),
+	date: z.string(),
+	link: z.string().url().optional()
+});
---
+++ src/routes/notes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { Note } from '$lib/components';
+	import { page } from '$app/state';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+{#if page.data.metadata}
+	<Note metadata={page.data.metadata}>
+		{@render children()}
+	</Note>
+{/if}
