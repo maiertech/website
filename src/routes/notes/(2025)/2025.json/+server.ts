5c892ef5d447296558710e34e6a87b5be1085ee5
Thilo Maier <git@maier.asia>
2025-06-18 17:53:01 +0200

    Add comment.

--- src/routes/notes/(2025)/2025.json/+server.ts
+++ src/routes/notes/(2025)/2025.json/+server.ts
@@ -6,5 +6,6 @@ import type { RequestHandler } from './$types';
 export const prerender = true;
 
 export const GET: RequestHandler = async () => {
+	// Sort order: latest first.
 	return json([howToWriteExceptionalDocs, envVarsInCode]);
 };
