35186e379d17d2b0cc13701bc5d76697c75cbba9
Thilo Maier <github@maier.asia>
2025-06-22 16:29:03 +0200

    Add note: The umpteenth interview with Rich Harris (#935)
    Fixes #934

--- src/routes/notes/api/2025/+server.ts
+++ src/routes/notes/api/2025/+server.ts
@@ -1,11 +1,16 @@
 import { json } from '@sveltejs/kit';
-import envVarsInCode from '../../(2025)/env-vars-in-vscode/meta.json';
-import howToWriteExceptionalDocs from '../../(2025)/how-to-write-exceptional-docs/meta.json';
+import noteEnvVarsInCode from '../../(2025)/env-vars-in-vscode/meta.json';
+import noteHowToWriteExceptionalDocs from '../../(2025)/how-to-write-exceptional-docs/meta.json';
 import type { RequestHandler } from './$types';
+import noteTheUmpteenthInterviewWithRichHarris from '../../(2025)/the-umpteenth-interview-with-rich-harris/meta.json';
 
 export const prerender = true;
 
 export const GET: RequestHandler = async () => {
 	// Sort order: latest first.
-	return json([howToWriteExceptionalDocs, envVarsInCode]);
+	return json([
+		noteTheUmpteenthInterviewWithRichHarris,
+		noteHowToWriteExceptionalDocs,
+		noteEnvVarsInCode
+	]);
 };
