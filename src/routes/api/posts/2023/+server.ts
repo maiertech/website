4a4f9ce04f6a92862fc09bce828648563af48e4e
Thilo Maier <git@maier.asia>
2025-06-28 14:14:20 +0200

    Migrate post

--- src/routes/api/posts/2023/+server.ts
+++ src/routes/api/posts/2023/+server.ts
@@ -1,12 +1,13 @@
 import { transformPost } from '$lib/server/transformations';
 import postComplementZeroEffortTypeSafety from '$posts/(2023)/complement-zero-effort-type-safety-in-sveltekit-with-zod-for-even-more-type-safety/meta';
 import postConfiguringTurborepo from '$posts/(2023)/configuring-turborepo-for-a-sveltekit-monorepo/meta';
+import postCookieSettingsForStackblitz from '$posts/(2023)/cookie-settings-for-stackblitz-webcontainers/meta';
+import postDoINeedASitemap from '$posts/(2023)/do-i-need-a-sitemap-for-my-sveltekit-app-and-how-do-i-create-it/meta';
 import postCustomizingGitpodWorkspaces from '$posts/(2023)/five-ways-to-customize-a-gitpod-workspace/meta';
 import postBasicSeoComponent from '$posts/(2023)/how-to-add-a-basic-seo-component-to-sveltekit/meta';
 import postStackBlitzCodeflowBeta from '$posts/(2023)/is-stackblitz-codeflow-beta-ready-to-replace-your-local-vs-code/meta';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import postDoINeedaSitemap from '$posts/(2023)/do-i-need-a-sitemap-for-my-sveltekit-app-and-how-do-i-create-it/meta';
 
 export const prerender = true;
 
@@ -17,7 +18,8 @@ export const GET: RequestHandler = async (event) => {
 		postCustomizingGitpodWorkspaces,
 		postConfiguringTurborepo,
 		postComplementZeroEffortTypeSafety,
-		postDoINeedaSitemap,
+		postDoINeedASitemap,
+		postCookieSettingsForStackblitz,
 		postStackBlitzCodeflowBeta
 	];
 
--- src/routes/posts/(2023)/cookie-settings-for-stackblitz-webcontainers/+page.svelte
+++ src/routes/posts/(2023)/cookie-settings-for-stackblitz-webcontainers/+page.svelte
@@ -5,28 +5,29 @@
 </script>
 
 <P>
-	More and more websites use StackBlitz WebContainers to embed live-code examples. The new Svelte
+	More and more websites use StackBlitz WebContainers to embed live code examples. The new Svelte
 	tutorial at <a href="https://learn.svelte.dev/">learn.svelte.dev</a> embeds StackBlitz
 	WebContainers to run Svelte and SvelteKit examples in supported browsers. Think of a WebContainer
-	as a small operating system that runs in a browser and can run Node.js version 16. Once you have
+	as a small operating system that runs in your browser and can run Node.js version 16. Once you have
 	Node.js running in your browser, it takes little effort to run entire Node-based stacks, e.g.,
-	<a href="https://kit.svelte.dev/">SvelteKit</a>, natively in a browser.
+	<a href="https://kit.svelte.dev/">SvelteKit</a>, natively in your browser.
 </P>
 
 <P>
 	I experimented with embedded WebContainers in my post
-	<a href="/posts/exploring-frequently-used-methods-of-d3-array"
-		>Exploring frequently used methods of d3-array</a
-	>. The first catch with WebContainers is that they are
-	<a href="https://developer.stackblitz.com/platform/webcontainers/browser-support"
-		>not yet supported in all browsers</a
-	>. Currently, Chrome and Firefox support WebContainers, but Safari does not.
+	<a href="/posts/exploring-frequently-used-methods-of-d3-array">
+		Exploring frequently used methods of d3-array
+	</a>. The first catch with WebContainers is that they are
+	<a href="https://developer.stackblitz.com/platform/webcontainers/browser-support">
+		not yet supported in all browsers.
+	</a>
+	Currently, Chrome and Firefox support WebContainers, but Safari does not.
 </P>
 
 <P>
 	The second catch is that embedded WebContainers need third-party cookies enabled to work. If your
 	audience is technical, there is a high chance that they are also security-conscious and have
-	third-party cookies disabled. When they see an embedded WebContainer in a browser, they see this
+	third-party cookies disabled. When they see an embedded WebContainer in their browser, they see this
 	warning:
 </P>
 
@@ -42,7 +43,7 @@
 
 <P>
 	The problem with this warning is that you cannot fix it for your users. Every user has to fix it
-	in their browser. Either by enabling third-party cookies for all sites or by always allowing
+	in their browser, either by enabling third-party cookies for all sites or by always allowing
 	cookies for these two sites:
 </P>
 
@@ -59,12 +60,12 @@
 <P>
 	For StackBlitz <a href="https://stackblitz.com/codeflow/beta">Codeflow Beta</a>, which I discussed
 	in my post
-	<a href="/posts/is-stackblitz-codeflow-beta-ready-to-replace-your-local-vs-code"
-		>Is StackBlitz Codeflow Beta ready to replace your local VSCode?</a
-	>, you have to add <Code>[*.]staticblitz.com</Code> to the above list to make it work without enabling
+	<a href="/posts/is-stackblitz-codeflow-beta-ready-to-replace-your-local-vs-code">
+		Is StackBlitz Codeflow Beta ready to replace your local VSCode?
+	</a>, you have to add <Code>[*.]staticblitz.com</Code> to the above list to make it work without enabling
 	third-party cookies. Check out the
-	<a href="https://developer.stackblitz.com/platform/webcontainers/browser-config"
-		>StackBlitz documentation</a
-	>
+	<a href="https://developer.stackblitz.com/platform/webcontainers/browser-config">
+		StackBlitz documentation
+	</a>
 	for more details on configuring third-party cookies in different browsers.
 </P>
