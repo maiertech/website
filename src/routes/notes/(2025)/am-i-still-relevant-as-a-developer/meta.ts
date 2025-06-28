97c7d636815e25ed749637fec4c10200cf9671a1
Thilo Maier <github@maier.asia>
2025-06-29 01:47:12 +0200

    Add description to note (#944)

--- src/routes/notes/(2025)/am-i-still-relevant-as-a-developer/meta.ts
+++ src/routes/notes/(2025)/am-i-still-relevant-as-a-developer/meta.ts
@@ -2,7 +2,8 @@ import type { NoteType } from '@maiertech/sveltekit-helpers';
 
 const meta: NoteType = {
 	title: 'Am I still relevant as a developer?',
-	description: '',
+	description:
+		'AI frees developers from chores, letting us focus on communication and code quality. Will this make us relevant enough?',
 	publishedDate: '2025-06-29',
 	link: 'https://github.blog/developer-skills/career-growth/why-developer-expertise-matters-more-than-ever-in-the-age-of-ai/',
 	path: '/notes/am-i-still-relevant-as-a-developer'
