6a7c0facc1a2f18278a1c1ae2d60bd18f47b4a9b
Thilo Maier <git@maier.asia>
2025-06-29 01:28:15 +0200

    Add note: Am I still relevant as a developer? Fixes #942

--- src/routes/api/notes/2025/+server.ts
+++ src/routes/api/notes/2025/+server.ts
@@ -1,3 +1,4 @@
+import noteAmIStillRelevantAsADeveloper from '$notes/(2025)/am-i-still-relevant-as-a-developer/meta';
 import noteEnvVarsInCode from '$notes/(2025)/env-vars-in-vscode/meta';
 import noteHowToWriteExceptionalDocs from '$notes/(2025)/how-to-write-exceptional-docs/meta';
 import noteTheUmpteenthInterviewWithRichHarris from '$notes/(2025)/the-umpteenth-interview-with-rich-harris/meta';
@@ -9,6 +10,7 @@ export const prerender = true;
 export const GET: RequestHandler = async () => {
 	// Sort order: latest first.
 	return json([
+		noteAmIStillRelevantAsADeveloper,
 		noteTheUmpteenthInterviewWithRichHarris,
 		noteHowToWriteExceptionalDocs,
 		noteEnvVarsInCode
---
+++ src/routes/notes/(2025)/am-i-still-relevant-as-a-developer/meta.ts
@@ -0,0 +1,11 @@
+import type { NoteType } from '@maiertech/sveltekit-helpers';
+
+const meta: NoteType = {
+	title: 'Am I still relevant as a developer?',
+	description: '',
+	publishedDate: '2025-06-29',
+	link: 'https://github.blog/developer-skills/career-growth/why-developer-expertise-matters-more-than-ever-in-the-age-of-ai/',
+	path: '/notes/am-i-still-relevant-as-a-developer'
+};
+
+export default meta;
