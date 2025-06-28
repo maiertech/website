8bd5cf3b39fe3070ea89cdc6e4ff1be1823aaf82
Thilo Maier <git@maier.asia>
2025-06-28 17:30:38 +0200

    Add notes to `sitemap.xml`

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,11 +1,13 @@
 import { PUBLIC_URL_ORIGIN } from '$env/static/public';
-import type { PostType } from '@maiertech/sveltekit-helpers';
+import type { PostType, NoteType } from '@maiertech/sveltekit-helpers';
 import type { RequestHandler } from './$types';
 
 export const prerender = true;
 
 export const GET: RequestHandler = async ({ fetch }) => {
-	const response = await fetch('/api/posts/all');
+	// Fetch posts.
+	// TODO: `/api/posts/all` does not include `lastmodDate`.
+	let response = await fetch('/api/posts/all');
 	const posts = (await response.json()) as PostType[];
 
 	// Create sitemap entries for posts.
@@ -16,8 +18,20 @@ export const GET: RequestHandler = async ({ fetch }) => {
 	</url>`
 	);
 
+	// Fetch notes.
+	response = await fetch('/api/notes/all');
+	const notes = (await response.json()) as NoteType[];
+
+	// Create sitemap entries for notes.
+	const noteEntries = notes.map(
+		(note) => `\t<url>
+		<loc>${new URL(note.path, PUBLIC_URL_ORIGIN).href}</loc>
+		<lastmod>${note.publishedDate}</lastmod>
+	</url>`
+	);
+
 	// Add additional entries to this array.
-	const pages = [...postEntries];
+	const pages = [...postEntries, ...noteEntries];
 
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
