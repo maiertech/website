0728e9d94433623358e558b2c73fdd371acbc10d
Thilo Maier <git@maier.asia>
2025-06-29 22:32:07 +0200

    Add note: Hosting in Europe beyond Big Tech Fixes #948

---
+++ src/routes/notes/(2025)/hosting-in-europe-beyond-big-tech/meta.ts
@@ -0,0 +1,12 @@
+import type { NoteType } from '@maiertech/sveltekit-helpers';
+
+// TODO: create description.
+
+const meta: NoteType = {
+	title: 'Hosting in Europe beyond Big Tech',
+	description: 'TODO',
+	publishedDate: '2025-07-01',
+	path: '/notes/hosting-in-europe-beyond-big-tech'
+};
+
+export default meta;
