e6ede3f4199fdc839b119784bc3ceecdb7b07d86
Thilo Maier <github@maier.asia>
2025-06-29 23:03:03 +0200

    Add description to note (#950)

--- src/routes/notes/(2025)/hosting-in-europe-beyond-big-tech/meta.ts
+++ src/routes/notes/(2025)/hosting-in-europe-beyond-big-tech/meta.ts
@@ -1,10 +1,9 @@
 import type { NoteType } from '@maiertech/sveltekit-helpers';
 
-// TODO: create description.
-
 const meta: NoteType = {
 	title: 'Hosting in Europe beyond Big Tech',
-	description: 'TODO',
+	description:
+		"Vercel boosted my web dev, but its Big Tech ties worry me. Now I'm searching for a true European alternative to Vercel's platform.",
 	publishedDate: '2025-06-30',
 	path: '/notes/hosting-in-europe-beyond-big-tech'
 };
