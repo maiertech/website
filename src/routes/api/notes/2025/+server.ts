4b76ab7c39e920b814bf0a5f7e377c4b7e3e0869
Thilo Maier <git@maier.asia>
2025-07-06 20:07:10 +0200

    Add note: Prerendering server routes in SvelteKit Fixes #954

--- src/routes/api/notes/2025/+server.ts
+++ src/routes/api/notes/2025/+server.ts
@@ -1,10 +1,11 @@
 import noteAmIStillRelevantAsADeveloper from '$notes/(2025)/am-i-still-relevant-as-a-developer/meta';
 import noteEnvVarsInCode from '$notes/(2025)/env-vars-in-vscode/meta';
+import noteHostingInEurope from '$notes/(2025)/hosting-in-europe-beyond-big-tech/meta';
 import noteHowToWriteExceptionalDocs from '$notes/(2025)/how-to-write-exceptional-docs/meta';
+import notePrerenderingServerRoutes from '$notes/(2025)/prerendering-server-routes-in-sveltekit/meta';
 import noteTheUmpteenthInterviewWithRichHarris from '$notes/(2025)/the-umpteenth-interview-with-rich-harris/meta';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import noteHostingInEurope from '$notes/(2025)/hosting-in-europe-beyond-big-tech/meta';
 
 // Needs to be set explicitly because we prerender endpoint `/sitemap.xml`.
 export const prerender = true;
@@ -12,6 +13,7 @@ export const prerender = true;
 export const GET: RequestHandler = async () => {
 	// Sort order: latest first.
 	return json([
+		notePrerenderingServerRoutes,
 		noteHostingInEurope,
 		noteAmIStillRelevantAsADeveloper,
 		noteTheUmpteenthInterviewWithRichHarris,
