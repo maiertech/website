b4155eee124417a14fe24eb13d02050ff4a89ce2
Thilo Maier <git@maier.asia>
2025-07-06 20:21:04 +0200

    Add missing SEO description

--- src/routes/notes/(2025)/prerendering-server-routes-in-sveltekit/meta.ts
+++ src/routes/notes/(2025)/prerendering-server-routes-in-sveltekit/meta.ts
@@ -2,7 +2,8 @@ import type { NoteType } from '@maiertech/sveltekit-helpers';
 
 export const meta: NoteType = {
 	title: 'Prerendering server routes in SvelteKit',
-	description: 'TODO',
+	description:
+		'SvelteKit auto-prerenders server routes, but manual `prerender = true` is needed for all server routes called by a prerendered endpoint.',
 	publishedDate: '2025-07-07',
 	link: 'https://svelte.dev/docs/kit/page-options#prerender-Prerendering-server-routes',
 	path: '/notes/prerendering-server-routes-in-sveltekit'
