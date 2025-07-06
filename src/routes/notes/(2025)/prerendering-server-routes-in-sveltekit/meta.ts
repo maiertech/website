6affe7af750e3d70f66a1702f2c43230c3b85263
Thilo Maier <github@maier.asia>
2025-07-06 20:11:37 +0200

    Add note: Prerendering server routes in SvelteKit (#955)
    Fixes #954

---
+++ src/routes/notes/(2025)/prerendering-server-routes-in-sveltekit/meta.ts
@@ -0,0 +1,11 @@
+import type { NoteType } from '@maiertech/sveltekit-helpers';
+
+export const meta: NoteType = {
+	title: 'Prerendering server routes in SvelteKit',
+	description: 'TODO',
+	publishedDate: '2025-07-07',
+	link: 'https://svelte.dev/docs/kit/page-options#prerender-Prerendering-server-routes',
+	path: '/notes/prerendering-server-routes-in-sveltekit'
+};
+
+export default meta;
