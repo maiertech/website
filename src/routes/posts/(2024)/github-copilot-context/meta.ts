e3e8733dc052321d045a4d6a19f837386058b5f0
Thilo Maier <git@maier.asia>
2025-07-27 11:05:28 +0200

    Add pre-generated OG image to latest post for testing

--- src/routes/posts/(2024)/github-copilot-context/meta.ts
+++ src/routes/posts/(2024)/github-copilot-context/meta.ts
@@ -1,4 +1,6 @@
 import type { PostMeta } from '@maiertech/sveltekit-helpers';
+import ogImageUrl from './og-image.png';
+import { PUBLIC_URL_ORIGIN } from '$env/static/public';
 
 export default {
 	title: 'How to provide better context in GitHub Copilot prompts',
@@ -8,5 +10,6 @@ export default {
 		'Learn to provide better context in GitHub Copilot prompts using inline chat, context shortcuts, chat participants, and project instructions for improved AI suggestions.',
 	tags: ['dx', 'ai', 'vscode'],
 	path: '/posts/github-copilot-context',
+	ogImageUrl: `${PUBLIC_URL_ORIGIN}${ogImageUrl}`,
 	filepath: 'src/routes/posts/(2024)/github-copilot-context/+page.svx'
 } satisfies PostMeta;
