6713d1812afb1ced92ed7b04afa24e800eb1083f
Thilo Maier <git@maier.asia>
2025-07-28 17:43:25 +0200

    Fix RSS feed

--- src/routes/posts/rss.xml/+server.ts
+++ src/routes/posts/rss.xml/+server.ts
@@ -11,6 +11,7 @@ export const GET: RequestHandler = async ({ fetch }) => {
 	const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
+    <atom:link href="${PUBLIC_URL_ORIGIN}/posts/rss.xml" rel="self" type="application/rss+xml" />
     <title>Thilo Maier (posts)</title>
     <description>RSS feed for Thilo Maier's posts.</description>
     <link>${PUBLIC_URL_ORIGIN}</link>
@@ -24,11 +25,11 @@ export const GET: RequestHandler = async ({ fetch }) => {
         <description>${post.description}</description>
         <link>${PUBLIC_URL_ORIGIN}${post.path}</link>
         <pubDate>${new Date(post.publishedDate).toUTCString()}</pubDate>
+        <guid>${PUBLIC_URL_ORIGIN}${post.path}</guid>
       </item>
     `
 			)
 			.join('')}
-      <atom:link href="${PUBLIC_URL_ORIGIN}/posts/rss.xml" rel="self" type="application/rss+xml" />
   </channel>
 </rss>`;
 
