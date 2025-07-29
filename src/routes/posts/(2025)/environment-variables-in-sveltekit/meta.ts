337e54dc2ad6e561214202b3c2664946508a7a67
Thilo Maier <git@maier.asia>
2025-07-30 00:30:36 +0200

    Update description

--- src/routes/posts/(2025)/environment-variables-in-sveltekit/meta.ts
+++ src/routes/posts/(2025)/environment-variables-in-sveltekit/meta.ts
@@ -4,7 +4,8 @@ export default {
 	title: 'Environment variables in SvelteKit',
 	author: 'thilo',
 	publishedDate: '2025-08-01',
-	description: 'TODO.',
+	description:
+		"Understand SvelteKit's four environment variable options and how to use them securely in different hosting and deployment scenarios.",
 	tags: ['svelte', 'vercel'],
 	path: '/posts/environment-variables-in-sveltekit',
 	filepath: 'src/routes/posts/(2025)/environment-variables-in-sveltekit/+page.svx'
