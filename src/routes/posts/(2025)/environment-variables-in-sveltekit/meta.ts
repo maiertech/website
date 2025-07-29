318415a38016b64680bc1b412471a1331e066bde
Thilo Maier <git@maier.asia>
2025-07-30 00:19:04 +0200

    Add post: Environment variables in SvelteKit Fixes #985

---
+++ src/routes/posts/(2025)/environment-variables-in-sveltekit/meta.ts
@@ -0,0 +1,11 @@
+import type { PostMeta } from '@maiertech/sveltekit-helpers';
+
+export default {
+	title: 'Environment variables in SvelteKit',
+	author: 'thilo',
+	publishedDate: '2025-08-01',
+	description: 'TODO.',
+	tags: ['svelte', 'vercel'],
+	path: '/posts/environment-variables-in-sveltekit',
+	filepath: 'src/routes/posts/(2025)/environment-variables-in-sveltekit/+page.svx'
+} satisfies PostMeta;
