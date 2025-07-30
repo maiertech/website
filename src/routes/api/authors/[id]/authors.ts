7d04138e89a4132b0f8037021e876839456c0b0a
Thilo Maier <git@maier.asia>
2025-07-30 09:31:11 +0200

    Fix broken portrait image Fixes #988

--- src/routes/api/authors/[id]/authors.ts
+++ src/routes/api/authors/[id]/authors.ts
@@ -1,10 +1,14 @@
 import type { AvatarMeta } from '@maiertech/sveltekit-helpers';
+import { getFullOrigin } from '$lib/utils';
+import { VERCEL_URL } from '$env/static/private';
+
+const fullOrigin = getFullOrigin(VERCEL_URL);
 
 export default [
 	{
 		id: 'thilo',
 		name: 'Thilo Maier',
 		url: 'https://bsky.app/profile/maier.tech',
-		imageUrl: '/assets/portrait-thilo-maier.jpg'
+		imageUrl: `${fullOrigin}/assets/portrait-thilo-maier.jpg`
 	}
 ] satisfies AvatarMeta[];
