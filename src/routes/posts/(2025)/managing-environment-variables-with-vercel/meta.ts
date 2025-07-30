ab0d2491c226af8ad639010556d8dc4120584bd8
Thilo Maier <git@maier.asia>
2025-07-30 16:10:17 +0200

    Add post: Managing environment variables with Vercel Fixes #991

---
+++ src/routes/posts/(2025)/managing-environment-variables-with-vercel/meta.ts
@@ -0,0 +1,12 @@
+import type { PostMeta } from '@maiertech/sveltekit-helpers';
+
+export default {
+	title: 'Managing environment variables with Vercel',
+	author: 'thilo',
+	publishedDate: '2025-08-01',
+	description:
+		'Manage environment variables in Vercel for any project and environment, and easily sync them to your local .env file.',
+	tags: ['vercel', 'vscode'],
+	path: '/posts/managing-environment-variables-with-vercel',
+	filepath: 'src/routes/posts/(2025)/managing-environment-variables-with-vercel/+page.svx'
+} satisfies PostMeta;
