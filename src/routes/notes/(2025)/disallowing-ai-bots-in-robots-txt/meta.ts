11d5ee7d458d363e47c5119caaf8e0fc6edd0aaf
Thilo Maier <github@maier.asia>
2025-08-01 17:29:28 +0200

    Add note: Disallow AI bots in `robots.txt` (#976)
    Fixes #975

---
+++ src/routes/notes/(2025)/disallowing-ai-bots-in-robots-txt/meta.ts
@@ -0,0 +1,10 @@
+import type { NoteMeta } from '@maiertech/sveltekit-helpers';
+
+export default {
+	title: 'Disallowing AI Bots in robots.txt',
+	description:
+		'How and why I updated my robots.txt to block AI bots from crawling my personal website, while allowing search engines.',
+	publishedDate: '2025-08-01',
+	link: 'https://github.com/ai-robots-txt/ai.robots.txt',
+	path: '/notes/disallowing-ai-bots-in-robots-txt'
+} satisfies NoteMeta;
