7057c69d8715cd8059aa17ec085c28c3013ac2a6
Thilo Maier <git@maier.asia>
2025-08-05 12:30:58 +0200

    Add note: Perplexity AI caught in the act Fixes #1000

---
+++ src/routes/notes/(2025)/perplexity-ai-caught-in-the-act/meta.ts
@@ -0,0 +1,9 @@
+import type { NoteMeta } from '@maiertech/sveltekit-helpers';
+
+export default {
+	title: 'Perplexity AI caught in the act',
+	description: 'TODO',
+	publishedDate: '2025-08-05',
+	link: 'https://blog.cloudflare.com/perplexity-is-using-stealth-undeclared-crawlers-to-evade-website-no-crawl-directives/',
+	path: '/notes/perplexity-ai-caught-in-the-act'
+} satisfies NoteMeta;
