0bfcaffea3e45c053b3167e42c3ab580e7701420
Thilo Maier <git@maier.asia>
2025-08-05 12:37:45 +0200

    Fix description

--- src/routes/notes/(2025)/perplexity-ai-caught-in-the-act/meta.ts
+++ src/routes/notes/(2025)/perplexity-ai-caught-in-the-act/meta.ts
@@ -2,7 +2,8 @@ import type { NoteMeta } from '@maiertech/sveltekit-helpers';
 
 export default {
 	title: 'Perplexity AI caught in the act',
-	description: 'TODO',
+	description:
+		'Perplexity AI caught using stealth crawlers despite robots.txt rules. Cloudflare exposes the practice, raising concerns for AI industry trust.',
 	publishedDate: '2025-08-05',
 	link: 'https://blog.cloudflare.com/perplexity-is-using-stealth-undeclared-crawlers-to-evade-website-no-crawl-directives/',
 	path: '/notes/perplexity-ai-caught-in-the-act'
