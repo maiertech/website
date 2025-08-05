7774eed77a06da2695246468a47bc82e8106ad94
Thilo Maier <github@maier.asia>
2025-08-05 12:34:29 +0200

    Add note: Perplexity AI caught in the act (#1001)
    Fixes #1000

--- src/routes/api/notes/2025/+server.ts
+++ src/routes/api/notes/2025/+server.ts
@@ -5,6 +5,7 @@ import noteEnvVarsInCode from '$notes/(2025)/env-vars-in-vscode/meta';
 import noteHostingInEurope from '$notes/(2025)/hosting-in-europe-beyond-big-tech/meta';
 import noteHowToWriteExceptionalDocs from '$notes/(2025)/how-to-write-exceptional-docs/meta';
 import notePenpotsMissingSvgTextToPathFeature from '$notes/(2025)/penpots-missing-svg-text-to-path-feature/meta';
+import notePerplexityAiCaughtInTheAct from '$notes/(2025)/perplexity-ai-caught-in-the-act/meta';
 import notePrerenderingServerRoutes from '$notes/(2025)/prerendering-server-routes-in-sveltekit/meta';
 import noteTheUmpteenthInterviewWithRichHarris from '$notes/(2025)/the-umpteenth-interview-with-rich-harris/meta';
 import { json } from '@sveltejs/kit';
@@ -16,6 +17,7 @@ export const prerender = true;
 export const GET: RequestHandler = async () => {
 	// Sort order: latest first.
 	const notes = [
+		notePerplexityAiCaughtInTheAct,
 		noteDisallowingAiBotsInRobotsTxt,
 		notePenpotsMissingSvgTextToPathFeature,
 		notePrerenderingServerRoutes,
