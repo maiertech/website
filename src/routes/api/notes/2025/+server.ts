c292f9f9894f8d19a6f48d7fb897644a72bb5a82
Thilo Maier <git@maier.asia>
2025-08-10 22:38:36 +0200

    Add note: Using GitHub Copilot in the terminal Fixes #1011

--- src/routes/api/notes/2025/+server.ts
+++ src/routes/api/notes/2025/+server.ts
@@ -8,6 +8,7 @@ import notePenpotsMissingSvgTextToPathFeature from '$notes/(2025)/penpots-missin
 import notePerplexityAiCaughtInTheAct from '$notes/(2025)/perplexity-ai-caught-in-the-act/meta';
 import notePrerenderingServerRoutes from '$notes/(2025)/prerendering-server-routes-in-sveltekit/meta';
 import noteTheUmpteenthInterviewWithRichHarris from '$notes/(2025)/the-umpteenth-interview-with-rich-harris/meta';
+import noteUsingGitHubCopilotInTheTerminal from '$notes/(2025)/using-github-copilot-in-the-terminal/meta';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
@@ -17,6 +18,7 @@ export const prerender = true;
 export const GET: RequestHandler = async () => {
 	// Sort order: latest first.
 	const notes = [
+		noteUsingGitHubCopilotInTheTerminal,
 		notePerplexityAiCaughtInTheAct,
 		noteDisallowingAiBotsInRobotsTxt,
 		notePenpotsMissingSvgTextToPathFeature,
