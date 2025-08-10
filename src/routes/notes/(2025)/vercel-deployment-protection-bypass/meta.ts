44f97ebcd14608c6435b1ed44d75f7fa95c5d574
Thilo Maier <git@maier.asia>
2025-08-11 00:20:48 +0200

    Add note: Vercel deployment protection bypass Fixes #1012

---
+++ src/routes/notes/(2025)/vercel-deployment-protection-bypass/meta.ts
@@ -0,0 +1,9 @@
+import type { NoteMeta } from '@maiertech/sveltekit-helpers';
+
+export default {
+	title: 'Vercel deployment protection bypass',
+	description: 'TODO',
+	publishedDate: '2025-08-15',
+	link: 'https://vercel.com/docs/deployment-protection/methods-to-bypass-deployment-protection/protection-bypass-automation',
+	path: '/notes/vercel-deployment-protection-bypass'
+} satisfies NoteMeta;
