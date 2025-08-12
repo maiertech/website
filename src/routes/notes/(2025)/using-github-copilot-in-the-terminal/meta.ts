259aab88314d6e9e0c544e25ecca57568893b792
Thilo Maier <github@maier.asia>
2025-08-12 18:04:02 +0200

    Add note: Using GitHub Copilot in the terminal (#1013)
    Fixes #1011

---
+++ src/routes/notes/(2025)/using-github-copilot-in-the-terminal/meta.ts
@@ -0,0 +1,10 @@
+import type { NoteMeta } from '@maiertech/sveltekit-helpers';
+
+export default {
+	title: 'Using GitHub Copilot in the terminal',
+	description:
+		'Add GitHub Copilot support to any terminal with the GitHub CLI to enable command suggestions and explanations.',
+	publishedDate: '2025-08-12',
+	link: 'https://docs.github.com/en/copilot/how-tos/use-copilot-for-common-tasks/use-copilot-in-the-cli',
+	path: '/notes/using-github-copilot-in-the-terminal'
+} satisfies NoteMeta;
