4dcb622466cf144978bafd3c4397c049df28b6d5
Thilo Maier <git@maier.asia>
2025-10-07 13:27:55 +0200

    Replace `VercelImage` with `enhanced:img`

---
+++ src/routes/posts/(2025)/dropping-requests-in-sveltekit/ProbingBotsImage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import srcProbingBots from './probing-bots.png?enhanced';
+</script>
+
+<enhanced:img
+	src={srcProbingBots}
+	alt="Log entries on Railway.com showing bot requests to potentially exposed files, for example, `/.env`."
+/>
