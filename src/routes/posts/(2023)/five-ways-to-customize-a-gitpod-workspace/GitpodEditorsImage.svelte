69cf971dbb98f5b457e9c6c4632b7afb7b63ba98
Thilo Maier <git@maier.asia>
2025-10-08 21:03:07 +0200

    Migrate 2 more posts

---
+++ src/routes/posts/(2023)/five-ways-to-customize-a-gitpod-workspace/GitpodEditorsImage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import srcGitpodEditors from './gitpod-editors.png?enhanced';
+</script>
+
+<enhanced:img
+	src={srcGitpodEditors}
+	alt="Screenshot of user settings in the Gitpod dashboard. There are ten cards with ten editor options, VS Code desktop and browser, and several IDEs by JetBrains."
+/>
---
+++ src/routes/posts/(2023)/how-to-add-a-basic-seo-component-to-sveltekit/SeoDataFlowImage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import srcSeoDataFlow from './seo-data-flow.png?enhanced';
+</script>
+
+<enhanced:img
+	src={srcSeoDataFlow}
+	alt="Diagram of how data from load functions can be shared between a page and a layout top down or bottom up."
+/>
