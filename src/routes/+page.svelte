3974b7c33497eabba6a77b42c2b9873fd776b6e4
Thilo Maier <github@maier.asia>
2025-10-12 21:29:35 +0200

    Refactor `PostPreview` and related layouts (#1041)
    Fixes #1040

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,15 +6,21 @@
 	let { data }: PageProps = $props();
 </script>
 
+<h1 class="sr-only">Thilo Maier</h1>
+
 <section class="mb-12 max-w-[45ch] text-xl lg:text-2xl">
 	<p>{data.seo.description}</p>
 </section>
+
+<h2 class="mb-4 text-2xl font-bold">Notes</h2>
 
 <section class="mb-12 flex flex-wrap gap-4">
 	{#each data.notes as note (note.path)}
 		<NotePreview value={note}></NotePreview>
 	{/each}
 </section>
+
+<h2 class="mb-4 text-2xl font-bold">Posts</h2>
 
 <section class="mb-12">
 	<Posts posts={data.posts} />
