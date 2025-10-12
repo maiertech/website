8d56b2a3da1c4af1f8ba9034018e8b9dc5baa2de
Thilo Maier <git@maier.asia>
2025-10-12 16:22:59 +0200

    Prerender `/api/notes/2025`

--- src/routes/api/notes/2025/+server.ts
+++ src/routes/api/notes/2025/+server.ts
@@ -14,8 +14,7 @@ import noteVercelProtectionBypass from '$notes/(2025)/vercel-deployment-protecti
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-// Needs to be set explicitly because we prerender endpoint `/sitemap.xml`.
-// export const prerender = true;
+export const prerender = true;
 
 export const GET: RequestHandler = async () => {
 	// Sort order: latest first.
