f6802e0a1907d31fb08430188380b244f8706f70
Thilo Maier <github@maier.asia>
2025-10-12 22:31:19 +0200

    Increase contrast in `PostPreview` (#1046)
    * Increase contrast in `PostPreview`
    Fixes #1045
    
    * Fix lint error

--- src/lib/components/posts.svelte
+++ src/lib/components/posts.svelte
@@ -7,12 +7,20 @@
 	}
 
 	let { posts }: Props = $props();
+
+	// Remove path from tags to avoid nested anchor tags.
+	const postsWithoutTagPaths = $derived(
+		posts.map((post) => ({
+			...post,
+			tags: post.tags?.map(({ id, label }) => ({ id, label }))
+		}))
+	);
 </script>
 
 <div data-component="Posts" class="flex flex-col gap-10">
-	<PostPreview value={posts[0]} />
+	<PostPreview value={postsWithoutTagPaths[0]} />
 	<div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
-		{#each posts.slice(1) as post (post.path)}
+		{#each postsWithoutTagPaths.slice(1) as post (post.path)}
 			<PostPreview value={post} />
 		{/each}
 	</div>
