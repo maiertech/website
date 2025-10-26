d1d7f66db6d83535b6b5b42d5059d2b23ba313dd
Thilo Maier <github@maier.asia>
2025-10-26 14:25:24 +0100

    Convert post `complement-zero-effort-type-safety-in-sveltekit-with-zod-for-even-more-type-safety` to Markdown (#1067)
    Fixes #1066

---
+++ src/routes/posts/(2023)/complement-zero-effort-type-safety-in-sveltekit-with-zod-for-even-more-type-safety/Example2Image.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import src from './zod-type-inference-example-2.png?enhanced';
+</script>
+
+<enhanced:img
+	{src}
+	alt="Screenshot of the code snippet from above in Visual Studio Code. The mouse hovers over the validated `subscriber` variable, and a pop-up shows the object type inferred from the `EOSubscriber` Zod schema."
+/>
