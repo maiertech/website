36b8250333bc01015cb285c41b9027578538a9ac
Thilo Maier <github@maier.asia>
2025-10-26 10:19:15 +0100

    Convert post `move-your-ide-to-the-cloud-introduction-to-github-codespaces` to Markdown (#1064)
    Fixes #1063

--- src/routes/posts/(2021)/move-your-ide-to-the-cloud-introduction-to-github-codespaces/+page.server.ts
+++ src/routes/posts/(2021)/move-your-ide-to-the-cloud-introduction-to-github-codespaces/+page.server.ts
@@ -6,20 +6,5 @@ export const load: PageServerLoad = async (event) => {
 	const post = await resolvePost({ postMeta: meta, event });
 	const { title, description, ogImageUrl } = post;
 
-	return { examples, post, seo: { title, description, ogImageUrl } };
-};
-
-const examples = {
-	'devcontainer.json': `{
-	"name": "javascript-node",
-	"image": "mcr.microsoft.com/vscode/devcontainers/javascript-node:18",
-	"customizations": {
-		"vscode": {
-			"extensions": ["dbaeumer.vscode-eslint", "esbenp.prettier-vscode"]
-		}
-	},
-	"forwardPorts": [3000],
-	"postCreateCommand": "npm i",
-	"remoteUser": "node"
-}`
+	return { post, seo: { title, description, ogImageUrl } };
 };
