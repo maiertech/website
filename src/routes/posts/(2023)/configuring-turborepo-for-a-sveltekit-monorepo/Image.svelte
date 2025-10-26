4434e032dbeff41d9a65b5000e6dbde95397f11c
Thilo Maier <github@maier.asia>
2025-10-26 22:02:43 +0100

    Convert post `configuring-turborepo-for-a-sveltekit-monorepo` to Markdown (#1069)
    Fixes #1068

---
+++ src/routes/posts/(2023)/configuring-turborepo-for-a-sveltekit-monorepo/Image.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import src from './vercel-build-error.png?enhanced';
+</script>
+
+<enhanced:img
+	{src}
+	alt="Screenshot of the error log of a Vercel deploy. The error message reads: No output directory named 'public' found after the build completed."
+/>
