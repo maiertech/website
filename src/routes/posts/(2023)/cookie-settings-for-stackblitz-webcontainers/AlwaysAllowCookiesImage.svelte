f2b20e2b3c9e94c117fed8b4cf4bd200b703f9cf
Thilo Maier <git@maier.asia>
2025-10-27 09:32:41 +0100

    Convert post `cookie-settings-for-stackblitz-webcontainers` to Markdown Fixes #1070

---
+++ src/routes/posts/(2023)/cookie-settings-for-stackblitz-webcontainers/AlwaysAllowCookiesImage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import src from './always-allow-cookies-light.png?enhanced';
+</script>
+
+<enhanced:img
+	{src}
+	alt="Screenshot of the cookies settings in a Chromium-based browser. Section 'Sites that can always use cookies' lists [*.]stackblitz.io and [*.]webcontainer.io."
+/>
---
+++ src/routes/posts/(2023)/cookie-settings-for-stackblitz-webcontainers/CookiesNotEnabledImage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import src from './cookies-not-enabled.png?enhanced';
+</script>
+
+<enhanced:img
+	{src}
+	alt="Screenshot of an embedded StackBlitz WebContainer with warning message 'Enable third-party cookies'."
+/>
