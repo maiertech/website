57551cd20f6b3748a8b5664c3be428aee0586a9d
Thilo Maier <git@maier.asia>
2025-10-27 10:14:17 +0100

    Convert post `is-stackblitz-codeflow-beta-ready-to-replace-your-local-vs-code` to Markdown Fixes #1072

---
+++ src/routes/posts/(2023)/is-stackblitz-codeflow-beta-ready-to-replace-your-local-vs-code/CodeflowBotImage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import srcCodeflowBotLight from './codeflow-bot-light.png?enhanced';
+</script>
+
+<enhanced:img
+	src={srcCodeflowBotLight}
+	alt="In this screenshot, codeflowapp commented on a pull request on GitHub and added a button to launch a Codeflow workspace."
+/>
---
+++ src/routes/posts/(2023)/is-stackblitz-codeflow-beta-ready-to-replace-your-local-vs-code/UnsupportedExtensionsImage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import srcUnsupportedExtensions from './unsupported-extensions.png?enhanced';
+</script>
+
+<enhanced:img
+	src={srcUnsupportedExtensions}
+	alt="Screenshot from Codeflow showing a warning message that says that the Thunder Client and Grammarly extensions are not supported."
+/>
