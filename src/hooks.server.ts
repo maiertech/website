25e65c9294f00fa55c5f0d8284fbc438278fa0e1
Thilo Maier <github@maier.asia>
2025-11-07 15:11:31 +0100

    Remove redirect (#1089)

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,6 @@
 import type { Handle } from '@sveltejs/kit';
-import { redirect } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	// Redirect apex domain to www subdomain.
-	if (event.url.hostname === 'maier.tech') {
-		const redirectUrl = new URL(event.url);
-		redirectUrl.hostname = 'www.maier.tech';
-		redirect(301, redirectUrl.toString());
-	}
-
 	// Preload web fonts.
 	const response = await resolve(event, {
 		preload: ({ type, path }) => {
