f8a60ec896d941cb7efc4421123a26762e6160d9
Thilo Maier <git@maier.asia>
2025-11-24 12:14:35 +0100

    Update deps

--- src/lib/components/posts.svelte
+++ src/lib/components/posts.svelte
@@ -1,26 +1,18 @@
 <script lang="ts">
 	import { PostPreview } from '@maiertech/sveltekit-helpers';
-	import type { ResolvedPost } from '@maiertech/sveltekit-helpers';
+	import type { Post } from '@maiertech/sveltekit-helpers';
 
 	interface Props {
-		posts: ResolvedPost[];
+		posts: Post[];
 	}
 
 	let { posts }: Props = $props();
-
-	// Remove path from tags to avoid nested anchor tags.
-	const postsWithoutTagPaths = $derived(
-		posts.map((post) => ({
-			...post,
-			tags: post.tags?.map(({ id, label }) => ({ id, label }))
-		}))
-	);
 </script>
 
 <div data-component="Posts" class="flex flex-col gap-10">
-	<PostPreview value={postsWithoutTagPaths[0]} />
+	<PostPreview value={posts[0]} />
 	<div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
-		{#each postsWithoutTagPaths.slice(1) as post (post.path)}
+		{#each posts.slice(1) as post (post.path)}
 			<PostPreview value={post} />
 		{/each}
 	</div>
