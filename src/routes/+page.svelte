9120a3f68bd2ac8b9ff691d8e88526e7981e81fc
Thilo Maier <github@maier.asia>
2025-12-10 17:54:47 +0100

    Render posts and notes on homepage with grids (#1136)
    Fixes #1135

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { Posts } from '$lib/components';
-	import { NotePreview } from '@maiertech/sveltekit-helpers';
+	import { NotePreview, H2, PostPreview } from '@maiertech/sveltekit-helpers';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
@@ -12,16 +11,22 @@
 	<p>{data.seo.description}</p>
 </section>
 
-<h2 class="mb-4 text-2xl font-bold">Notes</h2>
+<H2>Posts</H2>
 
-<section class="mb-12 flex flex-wrap gap-4">
-	{#each data.notes as note (note.path)}
-		<NotePreview value={note}></NotePreview>
-	{/each}
+<section class="@container mb-12">
+	<div class="grid grid-cols-1 gap-6 @xl:grid-cols-2">
+		{#each data.posts as post (post.path)}
+			<PostPreview value={post} level={3} />
+		{/each}
+	</div>
 </section>
 
-<h2 class="mb-4 text-2xl font-bold">Posts</h2>
+<H2>Notes</H2>
 
-<section class="mb-12">
-	<Posts posts={data.posts} />
+<section class="@container mb-12">
+	<div class="grid grid-cols-1 gap-6 @lg:grid-cols-2 @4xl:grid-cols-3">
+		{#each data.notes as note (note.path)}
+			<NotePreview value={note} level={3} />
+		{/each}
+	</div>
 </section>
