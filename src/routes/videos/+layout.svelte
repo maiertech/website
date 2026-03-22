3c49e9e7cddd87447edc9183c0e868b6ff182f26
Thilo Maier <git@maier.asia>
2026-03-22 17:33:11 +0100

    Fix svelte-check errors

--- src/routes/videos/+layout.svelte
+++ src/routes/videos/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
+	import { VideoHeader, VideoPlayer } from '@maiertech/sveltekit-helpers';
 	import type { LayoutProps } from './$types';
-	import { Video, VideoHeader } from '@maiertech/sveltekit-helpers';
 
 	let { data, children }: LayoutProps = $props();
 </script>
 
 <VideoHeader value={data.video} class="mb-12" />
 
-<Video id={data.video.id} class="mb-8" />
+<VideoPlayer value={data.video} class="mb-8" />
 
 {@render children()}
