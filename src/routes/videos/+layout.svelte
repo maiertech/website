0dcb3734786c46c394bed9a70275de80ca036115
Thilo Maier <git@maier.asia>
2026-03-26 17:51:23 +0100

    Use `Button` from `@maiertech/sveltekit-helpers`

--- src/routes/videos/+layout.svelte
+++ src/routes/videos/+layout.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import {
-		SeoCanonicalUrl,
-		VideoHeader,
-		VideoPlayer,
-		WatchOnYouTubeButton
-	} from '@maiertech/sveltekit-helpers';
+	import { SeoCanonicalUrl, VideoHeader, VideoPlayer, Button } from '@maiertech/sveltekit-helpers';
+	import { siYoutube } from 'simple-icons';
 	import type { LayoutProps } from './$types';
 
 	let { data, children }: LayoutProps = $props();
@@ -16,6 +12,16 @@
 
 <VideoPlayer value={data.video} class="mb-8" />
 
-<WatchOnYouTubeButton value={data.video} class="mb-8" />
+<div class="mb-8 flex justify-end">
+	<Button href={`https://www.youtube.com/watch?v=${data.video.id}`}>
+		{#snippet icon()}
+			<svg class="block size-4" viewBox="0 0 24 24" fill="currentColor" role="img">
+				<path d={siYoutube.path} />
+				<title>{siYoutube.title}</title>
+			</svg>
+		{/snippet}
+		Watch on YouTube
+	</Button>
+</div>
 
 {@render children()}
