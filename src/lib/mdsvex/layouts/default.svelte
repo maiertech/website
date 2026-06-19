4a20071dbb6b04016e00403c53fadb22808e7d7d
Thilo Maier <git@maier.asia>
2026-06-19 17:03:26 +0200

    Fine-tune theme

--- src/lib/mdsvex/layouts/default.svelte
+++ src/lib/mdsvex/layouts/default.svelte
@@ -1,6 +1,7 @@
 <script lang="ts" module>
 	// Custom components for Markdown rendering
 	import {
+		A as a,
 		Blockquote as blockquote,
 		Code as code,
 		H1 as h1,
@@ -10,13 +11,14 @@
 		H5 as h5,
 		H6 as h6,
 		Ol as ol,
+		P as p,
 		Pre as pre,
 		Table as table,
 		Ul as ul
 	} from '@maiertech/sveltekit-helpers';
 
 	// eslint-disable-next-line no-import-assign
-	export { blockquote, code, h1, h2, h3, h4, h5, h6, ol, pre, table, ul };
+	export { a, blockquote, code, h1, h2, h3, h4, h5, h6, ol, p, pre, table, ul };
 </script>
 
 <script lang="ts">
